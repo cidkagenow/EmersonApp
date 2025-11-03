c415ce560828b61ac292e8e3fb0fe25cb41cfc17
emerson ramos arellano <cidkagenow@emersons-MacBook-Pro.local>
2025-11-03 11:52:12 +0800

    add helper and transfer ownership

--- packages/stylus/erc20-example/src/lib.rs
+++ packages/stylus/erc20-example/src/lib.rs
@@ -18,22 +18,41 @@ const DECIMALS: U8 = uint!(10_U8);
 
 #[entrypoint]
 #[storage]
-pub struct ArbiLevelToken {
+pub struct EMtoken {
     erc20: Erc20,
     metadata: Erc20Metadata,
     capped: Capped,
+    owner: Address,
+
 }
 
 #[public]
-impl ArbiLevelToken {
+impl EMtoken {
     #[constructor]
     pub fn constructor(&mut self, name: String, symbol: String, cap: U256) {
         self.metadata.constructor(name, symbol);
         self.capped.constructor(cap).unwrap();
+        self.owner = stylus_sdk::msg::sender();
+
     }
 
-    pub fn mint(&mut self, account: Address, value: U256) {
+    fn only_owner(&self) -> Result<(), String> {
+        if stylus_sdk::msg::sender() != self.owner {
+            Err("Only owner can call this function".into())
+        } else {
+            Ok(())
+        }
+    }
+    pub fn transfer_ownership(&mut self, new_owner: Address) -> Result<(), String> {
+        self.only_owner()?;
+        self.owner = new_owner;
+        Ok(())
+    }
+
+    pub fn mint(&mut self, account: Address, value: U256) -> Result<(), String> {
+        self.only_owner()?;
         self.erc20._mint(account, value).unwrap();
+        Ok(())
     }
 
     pub fn total_supply(&self) -> U256 {
